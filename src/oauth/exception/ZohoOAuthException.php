bf7b27469471274135d8e3c85c28e82ec35f5f73
Dennis <dansion84@gmail.com>
2024-08-07 21:22:35 +0200

    Update ZohoOAuthException.php

--- src/oauth/exception/ZohoOAuthException.php
+++ src/oauth/exception/ZohoOAuthException.php
@@ -6,21 +6,6 @@ class ZohoOAuthException extends \Exception
     
     protected $message = 'Unknown exception';
     
-    // Exception message
-    private $string;
-    
-    // Unknown
-    protected $code = 0;
-    
-    // User-defined exception code
-    protected $file;
-    
-    // Source filename of exception
-    protected $line;
-    
-    // Source line of exception
-    private $trace;
-    
     public function __construct($message = null, $code = 0)
     {
         if (! $message) {
